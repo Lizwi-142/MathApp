30238f51ad656349e0b607b77dda106e87c83916
Lizwu <lizwi16seeno17@gmail.com>
2025-04-22 14:07:38 +0200

    Changes

--- app/src/main/java/vcmsa/ci/mathapp/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mathapp/MainActivity.kt
@@ -3,6 +3,8 @@ package vcmsa.ci.mathapp
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -25,64 +27,65 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editTextText)
         val editTextText2 = findViewById<EditText>(R.id.editTextText2)
         val editTextText3 = findViewById<EditText>(R.id.editTextText3)
-        val editButton = findViewById<Button>(R.id.Button)
-        val edtsubtract = findViewById<Button>(R.id.edtsubtract)
-val edtMultiplication  = findViewById<Button>(R.id.edtMultiplication)
-val edtDivision = findViewById<Button>(R.id.edtDivision)
+        val radioAdd = findViewById<TextView>(R.id.radioAdd)
+        val radioSubtract = findViewById<TextView>(R.id.radioSubtract)
+        val radioMultiply = findViewById<TextView>(R.id.radioMultiply)
+        val radioDivide = findViewById<TextView>(R.id.radioDivide)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+       val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
 
-
-
-        //set lick listener for the editButton button
-        edtDivision.setOnClickListener {
-            val secondnumber2 = editText.text.toString().toDoubleOrNull()
-            val thirdnumber3 = editTextText2.text.toString().toDoubleOrNull()
-            if (secondnumber2 != null && thirdnumber3 != null) {
-                val Totalvalue =  thirdnumber3/ secondnumber2
-                editTextText3.setText(Totalvalue.toString())
-            } else {
-                Toast.makeText(this, "please enter valid Number", Toast.LENGTH_SHORT).show()
+         // setting what happens when the button is clicked
+btnCalculate.setOnClickListener {
+    //get the values from the edit text
+    val num1 = editText.text.toString().trim()
+    val num2 = editTextText2.text.toString().trim()
+//check if the value is empty
+    if (num1.toString().isEmpty() || num2.toString().isEmpty()) {
+        Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show()
+        return@setOnClickListener
+    }
+    //convert the values to integers
+    val num3 = num1.toInt()
+    val num4 = num2.toInt()
+    val selected = radioGroup.checkedRadioButtonId
+        //check if no radio button is selected
+    if (selected == -1) {
+        //if no radio button is select,show a toast message
+        Toast.makeText(this, "Please select an operation", Toast.LENGTH_SHORT).show()
+        return@setOnClickListener}
+        var result = 0
+        var operation = ""
+        if (selected == R.id.radioAdd) {
+            //if the add radio button is selected, perform addition
+            result = num3 + num4
+            operation = "Addition"
+            editTextText3.text = "The answer is $result"
+            Toast.makeText(this, "The answer is $result", Toast.LENGTH_SHORT).show()
+        } else if (selected == R.id.radioSubtract) {
+            //if the subtract radio button is selected, perform subtraction
+            result = num3 - num4
+            operation = "subtraction"
+            editTextText3.text = "The answer is $result"
+            Toast.makeText(this, "The answer is $result", Toast.LENGTH_SHORT).show()
+        } else if (selected == R.id.radioMultiply) {
+            //if the multiply radio button is selected, perform multiplication
+            result = num3 * num4
+            operation = "Addition"
+            editTextText3.text = "The answer is $result"
+            Toast.makeText(this, "The answer is $result", Toast.LENGTH_SHORT).show()
+        }else if (selected == R.id.radioDivide) {
+            //if the divide radio button is selected, perform division
+            result = num3 / num4
+            operation = "Divide"
+            editTextText3.text = "The answer is  $result"
+            Toast.makeText(this, "The answer is $result", Toast.LENGTH_SHORT).show()}
+            //if the value is zero, show a toast message
+            else{Toast.makeText(this, "Please enter a value", Toast.LENGTH_SHORT).show()}
             }
+
         }
+    }
 
 
 
-
-        //set lick listener for the editButton button
-        edtMultiplication.setOnClickListener {
-            val secondnumber = editText.text.toString().toDoubleOrNull()
-            val thirdnumber = editTextText2.text.toString().toDoubleOrNull()
-            if (secondnumber != null && thirdnumber != null) {
-                val Totalvalue = secondnumber * thirdnumber
-                editTextText3.setText(Totalvalue.toString())
-            } else {
-                Toast.makeText(this, "please enter valid Number", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-                //set lick listener for the editButton button
-                edtsubtract.setOnClickListener {
-                    val numberTwo = editText.text.toString().toDoubleOrNull()
-                    val numberThree = editTextText2.text.toString().toDoubleOrNull()
-                    if (numberTwo != null && numberThree != null) {
-                        val Totalvalue = numberTwo - numberThree
-                        editTextText3.setText(Totalvalue.toString())
-                    } else {
-                        Toast.makeText(this, "please enter valid Number", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-                //set lick listener for the editButton button
-                editButton.setOnClickListener {
-                    val number2 = editText.text.toString().toDoubleOrNull()
-                    val number3 = editTextText2.text.toString().toDoubleOrNull()
-                    if (number2 != null && number3 != null) {
-                        val Totalvalue = number2 + number3
-                        editTextText3.setText(Totalvalue.toString())
-                    } else {
-                        Toast.makeText(this, "please enter valid Number", Toast.LENGTH_SHORT).show()
-                    }
-
-                }
-            }
-        }
 
